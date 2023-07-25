a8c8228e15d081af4ebb9b32e18ab30d712695e5
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-07-25 15:50:34 -0400

    draft

--- source/includes/fundamentals/code-snippets/crud/change.rs
+++ source/includes/fundamentals/code-snippets/crud/change.rs
@@ -7,8 +7,9 @@ async fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
     let client = Client::with_uri_str(uri).await?;
 
-    // begin-update
     let my_coll: Collection<Document> = client.database("db").collection("employees");
+
+    // begin-update
     let update_doc =
         doc!(
         "$set": doc!{ "department": "Business Operations", 
@@ -19,6 +20,22 @@ async fn main() -> mongodb::error::Result<()> {
     let res = my_coll.update_many(doc! { "department": "Marketing" }, update_doc, None).await?;
     println!("Modified {} document(s)", res.modified_count);
     // end-update
+
+    // begin-replace
+    let replace_doc =
+        doc!(
+        "name": "Susan Lee",
+        "role": "Lead Consultant",
+        "team_members": vec! [ "Jill Gillison" ]
+    );
+
+    let res = my_coll.replace_one(doc! { "_id": 4501 }, replace_doc, None).await?;
+    println!(
+        "Matched {} document(s)\nModified {} document(s)",
+        res.matched_count,
+        res.modified_count
+    );
+    // end-replace
 
     Ok(())
 }
