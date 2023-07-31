51dbfb768938a0bf7464e00e51b772ba351080cc
Nick LDP <nick.lopezdelpino@mongodb.com>
2023-07-31 18:17:04 -0400

    post review

--- source/code-snippets/quick-start/connect-async.rs
+++ source/code-snippets/quick-start/connect-async.rs
@@ -13,9 +13,9 @@ async fn main() -> mongodb::error::Result<()> {
    let my_coll: Collection<Document> = client.database("sample_mflix").collection("movies");
 
    // Find the movie "The Perils of Pauline" in the "movies" collection
-   let my_movie = my_coll.find_one(doc! { "title" : "The Perils of Pauline" }, None).await?;
+   let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None).await?;
 
    // Print the document that contains the movie found
-   println!("{:?}", my_movie.unwrap());
+   println!("{}", serde_json::to_string_pretty(&my_movie).unwrap());
    Ok(())
 }
--- source/code-snippets/quick-start/connect-sync.rs
+++ source/code-snippets/quick-start/connect-sync.rs
@@ -1,19 +1,23 @@
-use mongodb::{ bson::doc, Client, Collection };
+use mongodb::{ bson::doc, sync::{Client} };
+use bson:: Document;
 
 fn main() -> mongodb::error::Result<()> {
    // Replace the placeholder with your Atlas connection string
    let uri = "<connection string>";
    
    // Create a new client and connect to the server
-   let client = Client::with_uri_str(uri)?;
+   let client = Client::with_uri_str(uri);
 
    // Get a handle on the "movies" collection in the "sample_mflix" database
-   let my_coll: Collection<Document> =  client.database("sample_mflix").collection("movies");
+   let database = client?.database("sample_mflix");
+   let my_coll = database.collection::<Document>("movies");
 
    // Find the movie "The Perils of Pauline" in the "movies" collection
    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None)?;
 
    // Print the document that contains the movie found
-   println!("{:?}", my_movie.unwrap());
+   println!("{}", serde_json::to_string_pretty(&my_movie).unwrap());
    Ok(())
 }
+ 
+
