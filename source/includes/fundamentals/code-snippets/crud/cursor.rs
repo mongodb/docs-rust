d2f298b1da8cdfa2244d028fc4d0f680ec7da9c6
Nick LDP <nick.lopezdelpino@mongodb.com>
2023-08-03 11:22:21 -0400

    DOCSP-30537: Access data from a Cursor pg

---
+++ source/includes/fundamentals/code-snippets/crud/cursor.rs
@@ -0,0 +1,48 @@
+use bson::Document;
+use futures::stream::{StreamExt, TryStreamExt};
+use mongodb::{ bson::doc, Client, Collection, options::{ FindOptions } };
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection string>";
+    let client = Client::with_uri_str(uri).await?;
+    let my_coll: Collection<Document> = client.database("db").collection("");
+    
+    //begin cursor def
+    let mut cursor = my_coll.find(None, None).await?;
+    //end cursor def
+
+    
+    //begin try next
+    while let Some(result) = cursor.try_next().await? {
+       let doc: Document = bson::from_document(result)?;
+       println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    }
+    //end try next
+
+    //begin-cursor-next
+    while let Some(result) = cursor.next().await {
+       let doc: Document = bson::from_document(result)?;
+       println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    } 
+    //end-cursor-next
+
+    //begin cursor collect
+    // Stream uses populate vector containing all documents in the cursor
+    let v: Vec<_> = cursor.collect().await;
+    // Print data in vector
+    for i in v.iter(){
+        let doc: Document = bson::from_document(i.clone().ok().unwrap())?;
+        println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    }
+    //end cursor collect
+
+    //begin try collect
+    // TryStream uses try_collect() and collects into a Result<Vec<T>>
+    let v: Vec<_> = cursor.try_collect().await?;
+    // Print data in vector
+    for i in v.iter(){
+        let doc: Document = bson::from_document(i.clone())?;
+        println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    }
+    //end try collect
