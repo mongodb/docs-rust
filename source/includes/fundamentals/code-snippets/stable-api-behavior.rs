2e52ecedc17fba473ec7709830857adacbd6f1ad
pierwill <pierwill@users.noreply.github.com>
2023-08-29 09:12:11 -0500

    Rm code comments

--- source/includes/fundamentals/code-snippets/stable-api-behavior.rs
+++ source/includes/fundamentals/code-snippets/stable-api-behavior.rs
@@ -5,12 +5,10 @@ use mongodb::{
 };
 
 fn main() -> mongodb::error::Result<()> {
-    // Replace the placeholder with your Atlas connection string
     let uri = "<connection string>";
     // start-stable-api-behavior
     let mut client_options = ClientOptions::parse(uri)?;
 
-    // Set the server_api field of the client_options object to Stable API version 1
     let server_api = ServerApi::builder()
         .version(ServerApiVersion::V1)
         .strict(true)
@@ -18,11 +16,9 @@ fn main() -> mongodb::error::Result<()> {
         .build();
     client_options.server_api = Some(server_api);
 
-    // Create a new client and connect to the server
     let client = Client::with_options(client_options)?;
     // end-stable-api-behavior
 
-    // Send a ping to confirm a successful connection
     client
         .database("admin")
         .run_command(doc! { "ping": 1 }, None)?;
