aeef7ca4a95c1abc6d81228cde6d5a3ed6ae3bfb
pierwill <pierwill@users.noreply.github.com>
2023-09-12 12:00:12 -0500

    Add include

---
+++ source/includes/fundamentals/code-snippets/performance-new-runtime.rs
@@ -0,0 +1,16 @@
+use tokio::runtime::Runtime;
+use once_cell::sync::Lazy;
+
+static CLIENT: Lazy<Client> = Lazy::new(|| {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        Client::with_uri_str("mongodb://example.com").await.unwrap()
+    })
+});
+
+// This will inconsistently fail.
+#[tokio::test]
+async fn test_list_dbs() -> Result<(), Box<dyn Error>> {
+    CLIENT.list_database_names(None, None).await?;
+    Ok(())
+}
