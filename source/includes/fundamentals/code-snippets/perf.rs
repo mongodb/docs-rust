0fde5db39d206beeded0a8bd9ca25b15565bc2cc
pierwill <pierwill@users.noreply.github.com>
2023-09-12 11:43:32 -0500

    Add first include

---
+++ source/includes/fundamentals/code-snippets/perf.rs
@@ -0,0 +1,12 @@
+
+
+async fn handle_request() -> Result<(), Box<dyn Error>> {
+       let client = Client::with_uri_str("mongodb://example.com").await?;
+       // Do something with the client
+       Ok(())
+   }
+
+   async fn handle_request(client: &Client) -> Result<(), Box<dyn Error>> {
+       // Do something with the client
+       Ok(())
+   }
