d40a31ab7618482abdc6a7dd1f943830068f261b
pierwill <pierwill@users.noreply.github.com>
2023-09-13 09:42:22 -0500

    Fix indent

--- source/includes/fundamentals/code-snippets/perf.rs
+++ source/includes/fundamentals/code-snippets/perf.rs
@@ -1,12 +1,10 @@
-
-
 async fn handle_request() -> Result<(), Box<dyn Error>> {
-       let client = Client::with_uri_str("mongodb://example.com").await?;
-       // Do something with the client
-       Ok(())
-   }
+    let client = Client::with_uri_str("mongodb://example.com").await?;
+    // Do something with the client
+    Ok(())
+}
 
-   async fn handle_request(client: &Client) -> Result<(), Box<dyn Error>> {
-       // Do something with the client
-       Ok(())
-   }
+async fn handle_request(client: &Client) -> Result<(), Box<dyn Error>> {
+    // Do something with the client
+    Ok(())
+}
