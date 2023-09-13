f09d9a1e736878e7c377e7f6a80c628d061d560f
pierwill <pierwill@users.noreply.github.com>
2023-09-13 10:01:20 -0500

    Add include

---
+++ source/includes/fundamentals/code-snippets/performance-parallel.rs
@@ -0,0 +1,11 @@
+let client = Client::with_uri_str("mongodb://example.com").await?;
+
+for i in 0..5 {
+    let client_ref = client.clone();
+
+    task::spawn(async move {
+        let collection = client_ref.database("items").collection::<Document>(&format!("coll{}", i));
+
+        // Do something with the collection
+    });
+}
