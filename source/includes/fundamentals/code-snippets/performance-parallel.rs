a0e7441a219f52babf9c4e4b388cc938ed99bd75
pierwill <pierwill@users.noreply.github.com>
2023-10-03 16:57:19 -0500

    Fix example

--- source/includes/fundamentals/code-snippets/performance-parallel.rs
+++ source/includes/fundamentals/code-snippets/performance-parallel.rs
@@ -3,12 +3,13 @@ let some_data = doc! { "title": "1984", "author": "George Orwell" };
 
 for i in 0..5 {
     let client_ref = client.clone();
+    let somedata_ref = some_data.clone();
 
     task::spawn(async move {
         let collection = client_ref
             .database("items")
             .collection::<Document>(&format!("coll{}", i));
 
-        collection.insert_one(some_data)
+        collection.insert_one(somedata_ref, None).await
     });
 }
