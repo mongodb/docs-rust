4fecf00a575029d6053f11ce3ed030080086fb30
norareidy <nora.reidy@mongodb.com>
2023-10-19 13:50:07 -0400

    rest of feedback + reworking code

--- source/includes/usage-examples/code-snippets/delete-many-sync.rs
+++ source/includes/usage-examples/code-snippets/delete-many-sync.rs
@@ -2,16 +2,38 @@ use mongodb::{
     bson::{doc, Document},
     sync::{Client, Collection}
 };
+use serde::{ Deserialize, Serialize };
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Restaurant {
+    name: String,
+    address: Address,
+    borough: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Address {
+    street: String,
+}
 
 fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
-    
     let client = Client::with_uri_str(uri)?;
-    let my_coll: Collection<Document> = client.database("sample_guides").collection("planets");
 
-    let filter = doc! { "orderFromSun": doc!{ "$lte": 3 } };
+    let my_coll: Collection<Restaurant> = client
+        .database("sample_restaurants")
+        .collection("restaurants");
+
+    let filter =
+        doc! { "$and": [
+           doc! { "borough": "Manhattan" },
+           doc! { "address.street": "Broadway" }
+       ]
+    };
 
     let result = my_coll.delete_many(filter, None)?;
+
     println!("Deleted documents: {}", result.deleted_count);
 
     Ok(())
