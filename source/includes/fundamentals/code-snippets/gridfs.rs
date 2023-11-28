488505c59149c7072c07b74ef59b17848f836923
norareidy <nora.reidy@mongodb.com>
2023-11-28 15:56:28 -0500

    feedback

--- source/includes/fundamentals/code-snippets/gridfs.rs
+++ source/includes/fundamentals/code-snippets/gridfs.rs
@@ -1,5 +1,6 @@
 use mongodb::{
     bson::{ Bson, doc, oid::ObjectId }, 
+    options::{ WriteConcern, GridFsBucketOptions },
     Client
 };
 use futures_util::{
@@ -10,6 +11,7 @@ use futures_util::{
 use std::{
     fs,
     str::FromStr,
+    time::Duration,
 };
 
 #[tokio::main]
@@ -23,49 +25,76 @@ async fn main() -> mongodb::error::Result<()> {
     // end-create
 
     // start-create-opts
+    let wc = WriteConcern::builder().w_timeout(Duration::new(5, 0)).build();
+    
     let opts = GridFsBucketOptions::builder()
         .bucket_name("my_bucket".to_string())
+        .write_concern(wc)
         .build();
-    let bucket = my_db.gridfs_bucket(opts);
+    let bucket_with_opts = my_db.gridfs_bucket(opts);
     // end-create-opts
 
     // start-upload
-    let filename = "example.txt";
-    let file_bytes = fs::read(filename).await?;
+    let bucket = my_db.gridfs_bucket(None);
+    let file_bytes = fs::read("example.txt").await?;
+
     let mut upload_stream = bucket.open_upload_stream("example", None);
     upload_stream.write_all(&file_bytes[..]).await?;
+
     println!("Document uploaded with ID: {}", upload_stream.id());
     upload_stream.close().await?;
     // end-upload
 
     // start-retrieve 
+    let bucket = my_db.gridfs_bucket(None);
     let filter = doc! {};
     let mut cursor = bucket.find(filter, None).await?;
+
     while let Some(result) = cursor.try_next().await? {
         println!("File length: {}\n", result.length);
     };
     // end-retrieve
 
     // start-download
+    let bucket = my_db.gridfs_bucket(None);
     let id = ObjectId::from_str("3289").expect("Could not convert to ObjectId");
     let mut buf = Vec::new();
+
     let mut download_stream = bucket.open_download_stream(Bson::ObjectId(id)).await?;
     let result = download_stream.read_to_end(&mut buf).await?;
+
     println!("{:?}", result);
     // end-download
 
     // start-rename
+    let bucket = my_db.gridfs_bucket(None);
     let id = ObjectId::from_str("3289").expect("Could not convert to ObjectId");
     let new_name = "new_file_name";
+
     bucket.rename(Bson::ObjectId(id), &new_name).await?;
     // end-rename
 
+    // start-rename-many
+    let bucket = my_db.gridfs_bucket(None);
+    let id1 = ObjectId::from_str("3290").expect("Could not convert to ObjectId");
+    let id2 = ObjectId::from_str("3291").expect("Could not convert to ObjectId");
+    let id3 = ObjectId::from_str("3292").expect("Could not convert to ObjectId");
+
+    let new_name = "files_with_same_name";
+
+    bucket.rename(Bson::ObjectId(id1), &new_name).await?;
+    bucket.rename(Bson::ObjectId(id2), &new_name).await?;
+    bucket.rename(Bson::ObjectId(id3), &new_name).await?;
+    // end-rename-many
+
     // start-delete-file
+    let bucket = my_db.gridfs_bucket(None);
     let id = ObjectId::from_str("3289").expect("Could not convert to ObjectId");
     bucket.delete(Bson::ObjectId(id)).await?;
     // end-delete-file
 
     // start-delete-bucket
+    let bucket = my_db.gridfs_bucket(None);
     bucket.drop().await?;
     // end-delete-bucket
 
